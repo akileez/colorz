9eb861d376ea32f7b3f5ce6c94515225ff00aa3f
akileez <git@keeda.net>
2015-10-16 04:33:37 -0400

    update index
    — incorporated ‘grays’ into styles object.
    — remove instance of Object.keys() and replaced with two looping
    structures
    — rename main object `colorz`
    — reformat expose function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * Licensed under the ISC license.
  */
 
-var ansi = {}
+var colorz = {}
 var styles = {
   // modifiers
   reset         : [0, 0],
@@ -18,6 +18,8 @@ var styles = {
   strikethrough : [9, 29],
   // foregrounds
   black         : [30, 39],
+  gray          : [30, 39],
+  grey          : [30, 39],
   red           : [31, 39],
   green         : [32, 39],
   yellow        : [33, 39],
@@ -27,6 +29,8 @@ var styles = {
   white         : [37, 39],
   // bright foregrounds
   brBlack       : [90, 39],
+  brGray        : [90, 39],
+  brGrey        : [90, 39],
   brRed         : [91, 39],
   brGreen       : [92, 39],
   brYellow      : [93, 39],
@@ -36,6 +40,8 @@ var styles = {
   brWhite       : [97, 39],
   // backgrounds
   bgBlack       : [40, 49],
+  bgGray        : [40, 49],
+  bgGrey        : [40, 49],
   bgRed         : [41, 49],
   bgGreen       : [42, 49],
   bgYellow      : [43, 49],
@@ -45,6 +51,8 @@ var styles = {
   bgWhite       : [47, 49],
   // bright backgrounds
   bbBlack       : [100, 49],
+  bbGray        : [100, 49],
+  bbGrey        : [100, 49],
   bbRed         : [101, 49],
   bbGreen       : [102, 49],
   bbYellow      : [103, 49],
@@ -52,28 +60,45 @@ var styles = {
   bbMagenta     : [105, 49],
   bbCyan        : [106, 49],
   bbWhite       : [107, 49]
-  // exposure
 }
 
-styles.grey = styles.gray = styles.brBlack
-styles.brGrey = styles.brGray = styles.brBlack
-styles.bgGrey = styles.bgGray = styles.bgBlack
-styles.bbGrey = styles.bbGray = styles.bbBlack
-
-Object.keys(styles).forEach(function (style) {
+forEach(keys(styles), function (style) {
   var open = ['\u001b[', styles[style][0], 'm'].join('')
   var clos = ['\u001b[', styles[style][1], 'm'].join('')
-  ansi[style] = function (msg) {
+
+  colorz[style] = function (msg) {
     return [open, msg, clos].join('')
   }
 })
 
-ansi.strip = function (str) {
+colorz.strip = function (str) {
   return str.replace(/(?:\u001b\[)\d+m/g, '')
 }
 
-ansi.expose = function (style) {
-  return [style, ': ', JSON.stringify(ansi[style](' ++string++ '))].join('')
+colorz.expose = function (style, str) {
+  var text = style + ': ' + JSON.stringify(colorz[style](str))
+  return text
 }
 
-module.exports = ansi
+function keys (obj) {
+  var result = []
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push(key)
+    }
+  }
+  return result
+}
+
+function forEach (arr, fn) {
+  var i = -1
+  var len = arr.length
+
+  while (++i < len) {
+    if (fn(arr[i], i, arr) === false) break
+  }
+}
+
+module.exports = colorz
